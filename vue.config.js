af2c5850ea85864460024bb0c8c68bf5884bff13
shigw <844910815@qq.com>
2019-08-24 20:13:50 +0800

    格式化

--- vue.config.js
+++ vue.config.js
@@ -1,8 +1,8 @@
-"use strict";
+'use strict';
 /*主要现象配置 */
 
-const path = require("path");
-const defaultSettings = require("./src/settings.js");
+const path = require('path');
+const defaultSettings = require('./src/settings.js');
 
 function resolve(dir) {
   return path.join(__dirname, dir);
@@ -10,7 +10,7 @@ function resolve(dir) {
 /**
  * 默认标题
  */
-const name = defaultSettings.title || "vue Element Admin"; // page title
+const name = defaultSettings.title || 'vue Element Admin'; // page title
 
 // If your port is set to 80,
 // use administrator privileges to execute the command line.
@@ -31,10 +31,10 @@ module.exports = {
    * In most cases please use '/' !!!
    * Detail: https://cli.vuejs.org/config/#publicpath
    */
-  publicPath: "/",
-  outputDir: "dist",
-  assetsDir: "static",
-  lintOnSave: process.env.NODE_ENV === "development",
+  publicPath: '/',
+  outputDir: 'dist',
+  assetsDir: 'static',
+  lintOnSave: process.env.NODE_ENV === 'development',
   productionSourceMap: true,
   // 服务器配置
   devServer: {
@@ -48,12 +48,11 @@ module.exports = {
       // change xxx-api/login => mock/login
       // detail: https://cli.vuejs.org/config/#devserver-proxy
       [process.env.VUE_APP_BASE_API]: {
-        target: `http://192.168.1.114:5001`,
+        target: `http://localhost:5001`,
         changeOrigin: true
-
       }
     },
-    after: require("./mock/mock-server.js")
+    after: require('./mock/mock-server.js')
   },
   configureWebpack: {
     // provide the app's title in webpack's name field, so that
@@ -61,36 +60,36 @@ module.exports = {
     name: name,
     resolve: {
       alias: {
-        "@": resolve("src")
+        '@': resolve('src')
       }
     }
   },
   chainWebpack(config) {
-    config.plugins.delete("preload"); // TODO: need test
-    config.plugins.delete("prefetch"); // TODO: need test
+    config.plugins.delete('preload'); // TODO: need test
+    config.plugins.delete('prefetch'); // TODO: need test
 
     // set svg-sprite-loader
     config.module
-      .rule("svg")
-      .exclude.add(resolve("src/icons"))
+      .rule('svg')
+      .exclude.add(resolve('src/icons'))
       .end();
     config.module
-      .rule("icons")
+      .rule('icons')
       .test(/\.svg$/)
-      .include.add(resolve("src/icons"))
+      .include.add(resolve('src/icons'))
       .end()
-      .use("svg-sprite-loader")
-      .loader("svg-sprite-loader")
+      .use('svg-sprite-loader')
+      .loader('svg-sprite-loader')
       .options({
-        symbolId: "icon-[name]"
+        symbolId: 'icon-[name]'
       })
       .end();
 
     // set preserveWhitespace
     config.module
-      .rule("vue")
-      .use("vue-loader")
-      .loader("vue-loader")
+      .rule('vue')
+      .use('vue-loader')
+      .loader('vue-loader')
       .tap(options => {
         options.compilerOptions.preserveWhitespace = true;
         return options;
@@ -99,15 +98,15 @@ module.exports = {
 
     config
       // https://webpack.js.org/configuration/devtool/#development
-      .when(process.env.NODE_ENV === "development", config =>
-        config.devtool("cheap-source-map")
+      .when(process.env.NODE_ENV === 'development', config =>
+        config.devtool('cheap-source-map')
       );
 
-    config.when(process.env.NODE_ENV !== "development", config => {
+    config.when(process.env.NODE_ENV !== 'development', config => {
       config
-        .plugin("ScriptExtHtmlWebpackPlugin")
-        .after("html")
-        .use("script-ext-html-webpack-plugin", [
+        .plugin('ScriptExtHtmlWebpackPlugin')
+        .after('html')
+        .use('script-ext-html-webpack-plugin', [
           {
             // `runtime` must same as runtimeChunk name. default is `runtime`
             inline: /runtime\..*\.js$/
@@ -115,29 +114,29 @@ module.exports = {
         ])
         .end();
       config.optimization.splitChunks({
-        chunks: "all",
+        chunks: 'all',
         cacheGroups: {
           libs: {
-            name: "chunk-libs",
+            name: 'chunk-libs',
             test: /[\\/]node_modules[\\/]/,
             priority: 10,
-            chunks: "initial" // only package third parties that are initially dependent
+            chunks: 'initial' // only package third parties that are initially dependent
           },
           elementUI: {
-            name: "chunk-elementUI", // split elementUI into a single package
+            name: 'chunk-elementUI', // split elementUI into a single package
             priority: 20, // the weight needs to be larger than libs and app or it will be packaged into libs or app
             test: /[\\/]node_modules[\\/]_?element-ui(.*)/ // in order to adapt to cnpm
           },
           commons: {
-            name: "chunk-commons",
-            test: resolve("src/components"), // can customize your rules
+            name: 'chunk-commons',
+            test: resolve('src/components'), // can customize your rules
             minChunks: 3, //  minimum common number
             priority: 5,
             reuseExistingChunk: true
           }
         }
       });
-      config.optimization.runtimeChunk("single");
+      config.optimization.runtimeChunk('single');
     });
   }
 };
